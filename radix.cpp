b8fb08cde5bd1647afd32d204482d2c101a9b63b
Mik Nery <nery.mik@gmail.com>
2016-10-11 21:01:59 -0300

    now, yes

--- radix.cpp
+++ radix.cpp
@@ -28,7 +28,7 @@ int getDigit(cousint, cousint);
 void bloodyFuck(int);
 
 int main() {
-    int unsorted[] = {034, 224, 421, 145, 564, 7234, 9234, 653, 43542, 335, 252, 1235, 652, 025};
+    int unsorted[] = {34, 224, 421, 145, 564, 7234, 9234, 653, 43542, 335, 252, 1235, 652, 25};
     printf("unsorted = ");
     for(ui i = 0; i < 14; i++) {
         printf("[%d] ", unsorted[i]);
@@ -38,22 +38,17 @@ int main() {
     ui coefficient = 0;
     ui bot = 0, top = 0;
     scanf("%d %d %d", &bot, &top, &coefficient);
-    // countingSort(unsorted, bot, top, coefficient);
-    countingSort(unsorted, 0, 4, 1000);
+    countingSort(unsorted, bot, top, coefficient);
+    // countingSort(unsorted, 0, 4, 1000);
 
-    printf("sorted = ");
+    printf("sorted   = ");
     for(ui i = 0; i < 14; i++) {
         printf("[%d] ", unsorted[i]);
     }
     printf("\n");
 
-
-    // getDigit(328);
-    // int number = 0, in = 0;
-    // scanf("%d %d", &number, &in);
     return 0;
 }
-// printf("%d\n", getDigit(number, in));
 
 void countingSort(int* bucket, cousint bot, cousint top, cousint coefficient) {
     //counting how many of each number
@@ -69,17 +64,20 @@ void countingSort(int* bucket, cousint bot, cousint top, cousint coefficient) {
         if (i > 0) {
             positions[i] = index[i] + positions[i - 1];
         } else {
-            positions[i] = index[i] - 1;
+            positions[i] = bot + index[i] - 1; // the position on index '0' start at bottom of interval
         }
     }
 
     // sorting bucket
-    int* aux = copyArr(bucket, top-bot + 1); // aux keep original bucket values so bucket can be directly modified
+    int* aux = copyArr(bucket, 14); // aux keep original bucket values so bucket can be directly modified
     int j = 0;
     for(ui i = bot; i <= top ; i++) {
         int current = aux[top - j++]; //current number beeing ordered from bucket array
+        // printf("current = %d // top = %d\n", top - j + 1, top);
         int result_position = positions[getDigit(current, coefficient)]--; //position in which 'current' will be put
         bucket[result_position] = current;
+        // for(ui i = 0; i < 14; i++) printf("[%d] ", bucket[i]);
+        // printf("\n");
     }
 }
 
